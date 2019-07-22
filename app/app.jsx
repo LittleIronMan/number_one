5bfad61c0579598f35a51a7e0e0fc8a16fd905a5
RoE_Mishka <mrstark375@gmail.com>
2019-07-22 15:43:52 +0700

    Исправил контейнер с ютубовским видео, теперь там нету всякой ютубовской хуеты, вроде таймлайна, рекомендуемых видео, и прочего. Красная кнопка тоже теперь тоже будет не видна, потому что курсор не сможет сфокусироваться на ней. Вообще весь контейнер с видео сверху будет загражден специальным прозрачным контейнером stub-youtube, у него z-index: 2 и он будет перехватывать движения курсора в зоне видео. Максимум на него можно нажать будет, чтобы остановить/продолжить видео(для этого для него прописан специальный обработчик кликов).

--- app/app.jsx
+++ app/app.jsx
@@ -14,16 +14,28 @@ const menuItems = {
 
 class ElyaTitle extends React.Component {
     render() {
-        return <div className="elyaVideoContainer">
+        return <div className="elyaVideo">
             {/*Youtube video*/}
-            <iframe
-                width="420"
-                height="315"
-                src="https://www.youtube.com/embed/cpSJF2FDP2A"
-                frameborder="0"
-                allowfullscreen
-                className="video">
-            </iframe>
+            <div className="stub-youtube"/>
+            <div id="elyaVideoPlayer" className="youtube"></div>
+            {/*<iframe*/}
+            {/*    // width="420"*/}
+            {/*    // height="315"*/}
+            {/*    // src="https://www.youtube.com/embed/cpSJF2FDP2A"*/}
+            {/*    // frameborder="0"*/}
+            {/*    // allowfullscreen*/}
+            {/*    // className="video"*/}
+
+            {/*    id="elyaVideoPlayer"*/}
+            {/*    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"*/}
+            {/*    title="YouTube video player" width="640" height="360"*/}
+            {/*    allowFullScreen*/}
+            {/*    className="youtube"*/}
+            {/*    // src="https://www.youtube.com/embed/cpSJF2FDP2A?autoplay=1&amp;controls=0&amp;rel=0&amp;enablejsapi=1&amp;origin=https%3A%2F%2Fen.past-indicator.com&amp;widgetid=1"*/}
+            {/*// чтобы видео заработало нужно будет обязательно передать в origin полный домен сайта*/}
+            {/*    src="https://www.youtube.com/embed/cpSJF2FDP2A"*/}
+            {/*    >*/}
+            {/*</iframe>*/}
         </div>;
     }
 }

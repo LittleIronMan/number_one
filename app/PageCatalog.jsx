6c24796153b80d74033d9d4a7157a653315b1fa4
RoE_Mishka <mrstark375@gmail.com>
2019-07-26 09:58:37 +0700

    Константные массивы вытащил из файлов модулей, они и так должны быть глобальными

--- app/PageCatalog.jsx
+++ app/PageCatalog.jsx
@@ -3,32 +3,6 @@ var ElyaMenu = require('./components/ElyaMenu.jsx');
 var {ElyaNavbar, ElyaTitleYoutube, ElyaFooter} = require('./components/ElyaUI.jsx');
 var ElyaCatalogItem = require('./components/ElyaCatalogItem.jsx');
 
-const menuItems = [
-    {text: "main", path: "/"},
-    {text: "catalog", path: "/catalog"},
-    {text: "package", path: "/"},
-    {text: "delivery", path: "/"},
-    {text: "about", path: "/"}
-]
-
-const catalogItems = [
-    {name: "first", price: "$100",
-        images: [
-            "../img/45_2.jpg",
-            "../img/46_2.jpg",
-            "../img/49_2.jpg",
-            "../img/50_2.jpg"
-        ]
-    },
-    {name: "second", price: "$200",
-        images: [
-            "../img/47_2.jpg",
-            "../img/48_2.jpg",
-            "../img/52_2.jpg"
-        ]
-    }
-]
-
 class ElyaCatalog extends React.Component {
     render() {
         return <div className="elyaCatalogWrapper">
--- app/PageMain.jsx
+++ app/PageMain.jsx
@@ -2,14 +2,6 @@ var React = require('react');
 var ElyaMenu = require('./components/ElyaMenu.jsx');
 var {ElyaNavbar, ElyaTitleYoutube, ElyaFooter} = require('./components/ElyaUI.jsx');
 
-const menuItems = [
-    {text: "main", path: "/"},
-    {text: "catalog", path: "/catalog"},
-    {text: "package", path: "/"},
-    {text: "delivery", path: "/"},
-    {text: "about", path: "/"}
-]
-
 class ElyaCatalogMini extends React.Component {
     render() {
         return <div className="elyaCatalog">

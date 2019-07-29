71f69229b3e9ac67d2da09abf6f169183c86015f
RoE_Mishka <mrstark375@gmail.com>
2019-07-29 12:32:26 +0700

    Много правок, и то не доделал что хотел. Просто сохранение чтобы откатить можно было. Попробовал реализовать карусель для элементов каталога с возможностью свайпа, добавил библиотеки react-swipeable и react-transition-group.

--- app/components/ElyaCatalogItem.jsx
+++ app/components/ElyaCatalogItem.jsx
@@ -1,44 +1,100 @@
 'use strict';
 var React = require('react');
 var {Redirect} = require('react-router-dom');
+var {Motion, spring} = require('react-motion');
+var {Swipeable} = require('react-swipeable');
+var CSSTransition = require('react-transition-group').CSSTransition;
+
+// var styles = require('../../stylesheets/screen.css');
 
 class ElyaCatalogItem extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {selectedImage: 0, redirect: false}; // по-умолчанию показывается первое изображение
+        this.state = {selectedImage: 0, redirect: false, showSliderArrows: false}; // по-умолчанию показывается первое изображение
         this.onClick = this.onClick.bind(this);
+        this.onMouseDown = this.onMouseDown.bind(this);
     }
-    onClick() {
-        // some action...
-        // then redirect
-        this.setState({redirect: true});
+    onMouseDown(event) {
+        this.x = event.screenX;
+    }
+    onClick(event) {
+        let dist = Math.abs(this.x - event.screenX);
+        if (dist > 10) {
+            // ignore
+        }
+        else {
+            this.setState({redirect: true}); // then redirect
+        }
+        this.x = 0;
     };
+    switchImg(dir) {
+        let newIdx = this.state.selectedImage + dir;
+        let last = this.props.item.images.length - 1;
+        if (newIdx > last) { newIdx = 0; }
+        else if (newIdx < 0) { newIdx = last; }
+        this.setState({...this.state, selectedImage: newIdx});
+    }
     render() {
+        console.log("Render item, showSliderArrows = ", this.state.showSliderArrows);
         if (this.state.redirect) {
             return <Redirect push to={`/product/${this.props.item.name}`} />;
         }
         else {
             let images = this.props.item.images;
             let id = this.props.item.name;
-            let focusWidth = 100 / images.length;
+            let sliderState = {
+                selectedImgSpring: spring(this.state.selectedImage)
+            }
+            const swipeConfig = {
+                onSwipedLeft: (eventData) => this.switchImg(1),
+                onSwipedRight: (eventData) => this.switchImg(-1),
+                preventDefaultTouchmoveEvent: true,
+                trackMouse: true
+            };
+            const sliderArrows = ["left", "right"];
             return <div className="elyaCatalogItem col-6 col-lg-4">
                 <div className="context">
-                    <div className="img-container" onClick={this.onClick}>
-                        {images.map((img, index) =>
-                            <div className="img" id={id + index} style={{
-                                backgroundImage: `url("${img}")`,
-                                //selectedImage будет отрисовываться поверх других изображений
-                                zIndex: `${index === this.state.selectedImage ? 10 : 1}`
-                            }}
-                            />)}
-                        {images.map((img, index) =>
-                            <div className={`focus-capture${index === this.state.selectedImage ? " active" : ""}`}
-                                 style={{width: `${focusWidth}%`, left: `${focusWidth * index}%`}}
-                                 onMouseOver={() => {
-                                     this.setState({selectedImage: index});
-                                 }}
-                            />)}
-                    </div>
+                    <Swipeable {...swipeConfig}>
+                        <Motion style={sliderState}>
+                            {({selectedImgSpring}) =>
+                                    <div className="img-container" onClick={this.onClick} onMouseDown={this.onMouseDown}
+                                        onMouseOver={() => { console.log("onMouseOver"); this.setState({...this.state, showSliderArrows: true})}}
+                                        onMouseLeave={() => { console.log("onMouseLeave"); this.setState({...this.state, showSliderArrows: false})}}
+                                    >
+                                        {images.map((img, index) =>
+                                            <div className="img" id={id + index} style={{
+                                                backgroundImage: `url("${img}")`,
+                                                //selectedImage будет отрисовываться поверх других изображений
+                                                // zIndex: `${index === this.state.selectedImage ? 10 : 1}`,
+                                                left: `${(index - selectedImgSpring) * 100}%`
+                                            }}
+                                            />
+                                        )}
+                                        <CSSTransition
+                                            in={this.state.showSliderArrows}
+                                            // unmountOnExit="true"
+                                            timeout={300}
+                                            classNames="slider-arrows"
+                                        >
+                                            <div className="slider-arrows">
+                                                {sliderArrows.map((dir) =>
+                                                    <div className={`arrow ${dir}`}
+                                                         onClick={(e) => {
+                                                             if (this.state.showSliderArrows) {
+                                                                 this.switchImg(-1);
+                                                                 e.stopPropagation();
+                                                             }
+                                                         }}
+                                                    >
+                                                        <i className={`icon fa fa-arrow-${dir} fa-fw`}></i>
+                                                    </div>
+                                                )}
+                                            </div>
+                                        </CSSTransition>
+                                    </div>
+                            }
+                        </Motion>
+                    </Swipeable>
 
                     <hr/>
                     <div className="footer">
